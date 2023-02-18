27a155787b3de9ad7a4cf5f794182938dd361372
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-02-18 14:07:15 -0500

    Add additional Tokens to the scanner (number, string); add Peek methods.

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -83,9 +83,75 @@ public class Scanner {
             case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
+            case '/':
+                if (match('/')) {
+                    // comment is all contents until the end of the line; consume the entire line
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+            case ' ':
+            case '\r':
+            case '\t':
+                // ignore whitespace
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
             default:
-                Lox.error(line, String.format("Unexpected Character: %c", c));
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lox.error(line, String.format("Unexpected Character: %c", c));
+                }
         }
+    }
+
+    private void number() {
+        // consume all characters while the next character is a digit
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        // determine if floating point literal
+        if (peek() == '.' && isDigit(peekNext())) {
+            // we'll consume the `.`
+            advance();
+            // consume characters after the `.` for the remaining digits in the float literal
+            while (isDigit(peek())) {
+                advance();
+            }
+        }
+
+        // add the number now that all digits have been collected
+        final Double tokenValue = Double.parseDouble(source.substring(start, current));
+        addToken(NUMBER, tokenValue);
+    }
+
+    private void string() {
+        // consume characters until the end of the string
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated String");
+            return;
+        }
+
+        // consume the closing "
+        advance();
+
+        // remove the quotes from the string
+        // as `start` point to the 1st character in the lexeme, and we've consumed the last character
+        final String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     /**
@@ -102,6 +168,32 @@ public class Scanner {
 
         current++;
         return true;
+    }
+
+    /**
+     * Perform lookahead for a single, current character (do not consume the character)
+     *
+     * @return the next character
+     */
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    /**
+     * Perform lookahead (do not consume) for an additional character (one greater than current)
+     *
+     * @return the character one index greater than the current index in the source string
+     */
+    private char peekNext() {
+        if (current + 1 > source.length()) {
+            return '\0';
+        }
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return '0' <= c && '9' >= c;
     }
 
     /**
